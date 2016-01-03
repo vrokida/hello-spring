10f884fe54441fe3be783579ccaea8124a214dc6
vrokida <vrokida@gmail.com>
2016-01-03 12:11:03 -0500

    add sysout

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -13,10 +13,12 @@ public class DemoApplication {
 	@RequestMapping("/")
 	@ResponseBody
 	String home() {
+		System.out.println("***********************************vero****************************");
 		return "Hello World!";
 	}
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
